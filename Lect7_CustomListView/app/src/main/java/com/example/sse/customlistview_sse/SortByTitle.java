c92b83fba8b06f83a4f8d5ab0dade579a3cebd88
RayRedeemer <supercontra@outlook.com>
2019-10-22 09:37:34 +0800

    finished sorting by title

--- Lect7_CustomListView/app/src/main/java/com/example/sse/customlistview_sse/SortByTitle.java
+++ Lect7_CustomListView/app/src/main/java/com/example/sse/customlistview_sse/SortByTitle.java
@@ -7,15 +7,11 @@ import android.view.ViewGroup;
 
 public class SortByTitle extends MyCustomAdapter {
     private int ints[];
+    private String title[];
 
     public SortByTitle(Context aContext, SharedPreferences sp) {
         super(aContext, sp);
-
-    }
-
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        String[] title = getEpisodes();
+        title = getEpisodes().clone();
         ints = new int[title.length];
         for(int i = 0; i < ints.length; i++){
             ints[i] = i;
@@ -23,12 +19,19 @@ public class SortByTitle extends MyCustomAdapter {
         for(int i = 0; i < ints.length; i++){
             for(int j = 0; j < ints.length - 1; j++){
                 if( title[j].compareTo(title[j+1]) > 0 ){
+                    String str = title[j];
+                    title[j] = title[j+1];
+                    title[j+1] = str;
                     int temp = ints[j];
                     ints[j] = ints[j+1];
                     ints[j+1] = temp;
                 }
             }
         }
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
         position = ints[position];
         return super.getView(position, convertView, parent);
     }
